bb271d5531c3c45c72596731b85cf2d699c1e94e
Carlos Jimenez <cjimenez02@gmail.com>
2019-05-02 12:08:43 -0400

    uptade homebrew

--- muter.rb
+++ muter.rb
@@ -1,7 +1,8 @@
 class Muter_TEST < Formula
 	desc "Automated mutation testing for Swift"
 	homepage "https://github.com/polar133/muter"
-	url "https://github.com/polar133/muter"
+	url "https://github.com/polar133/muter", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+    head "https://github.com/polar133/muter.git"
 	
 	def install
 		system "make", "install", "prefix=#{prefix}"
